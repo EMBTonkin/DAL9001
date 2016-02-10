269b9199e47489a4d874c5fe7ba74c7a5d64d05a
EMBTonkin <LizzieTonkin@gmail.com>
2016-02-09 22:22:03 -0500

    Began User Interface
    Began constructing a basic user interface.
    Currently consists of a window with three panels in it.
    
    Two panels live in the Display object, which is the view of our MVC.
    Will have lots of functions to update and change what is displayed.
    
    DAL9001 object creates the window and a side panel with buttons in it.
    Buttons as of yet do nothing, but this will be the controller/main file when that day rolls around.

--- code/DAL9001.java
+++ code/DAL9001.java
@@ -9,3 +9,82 @@
 	// Model-View-Controller (whatever that means)
 	// Get and Set functions (don't directly edit an object's data)
 	// Avoid the singularity (no malevolent sentient programs here)
+	
+	
+// need to sort through these later and see what is actually being used. -LT
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+
+
+/**
+ * Displays Dragon stuff graphically using Swing. 
+ * Based on series of projects from CS 231.
+ * @author Lizzie Tonkin, lovingly ripped off of bseastwo and professor Kyle Burke
+ */
+public class DAL9001 extends JFrame
+{
+	private Display canvas;
+	
+	/**
+	 * Initializes a display window.  But soon, so much more!
+	 */
+	public DAL9001()
+	{
+		// setup the window
+		super("DAL9001");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		
+		// create a panel which will be the View of our MVC
+		this.canvas = new Display();
+		BoxLayout boxLayout = new BoxLayout(this.getContentPane(), BoxLayout.X_AXIS); // top to bottom
+    	this.setLayout(boxLayout);
+		
+		// create the side bar, which will be the Controller of our MVC 
+		
+		JButton reset = new JButton("Reset");
+		JButton quit = new JButton("Quit");
+		JLabel status = new JLabel("Text is here, this can be changed");
+		
+		JPanel panel = new JPanel();
+		
+		panel.add(quit);
+		panel.add(reset);
+		panel.add(status);
+		panel.setPreferredSize(new Dimension(200, 1000)); // nessasarry to set all three for a fixed size.
+		panel.setMaximumSize(new Dimension(200, 1000));
+		panel.setMinimumSize(new Dimension(200, 1000));
+		panel.setBackground(new Color(255,0,0));
+		
+		
+		// bring our beautiful creation together.
+		this.add(panel); 
+		this.add(this.canvas);
+		
+		this.pack();
+		this.setVisible(true);
+		
+		Graphics g = canvas.getGraphics();
+		canvas.update( g );
+		
+	}
+	
+	
+		
+	// Main function
+	// Runs the program, also used for testing since we are testing UI.
+	public static void main(String[] args)  {
+		DAL9001 thing = new DAL9001();
+	}
+}
--- code/Display.java
+++ code/Display.java
@@ -3,3 +3,139 @@
 // If you finish all the well outlined file's first though, feel free to take a stab!
 
 /// so is this the 'view' or 'controller'?  -SS2
+/// It's the view. -LT
+
+
+// imports
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+
+
+
+/**
+ * The Display class is the View of the model view controller
+ * Will have various functions to visualize data generated/ requested by the user.
+ */
+class Display extends JPanel{		
+	
+	private HeaderPanel header;
+	private DisplayPanel panel;
+	
+	/**
+	 * Constructor creates a JPanel with two sub panels,
+	 * 		the DisplayPanel which is where the dragon tree visualization lives
+	 * 		the HeaderPanel which is where specific dragon data is displayed
+	 */
+	public Display(){
+		super();
+		
+		panel = new DisplayPanel();
+		header = new HeaderPanel();
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+   		this.add(header); 
+		this.add(panel); 
+		
+	}	
+	
+	
+	/**
+	 * Updates the display
+	 */
+	public void update() {
+		Graphics g = panel.getGraphics();
+		panel.paintComponent( g );
+		Graphics g2 = header.getGraphics();
+		header.paintComponent( g2 );
+		this.requestFocus();
+	}
+	
+	
+	/**
+	 * This inner class provides the panel on which dragon family elements
+	 * are drawn.
+	 */
+	private class DisplayPanel extends JPanel{
+		/**
+		 * Creates the panel
+		 */
+		public DisplayPanel(){
+			super();
+			this.setPreferredSize(new Dimension(1250, 750));
+			this.setBackground(new Color(100,100,0));
+		}
+
+		/**
+		 * Method overridden from JComponent that is responsible for
+		 * drawing components on the screen.  The supplied Graphics
+		 * object is used to draw.
+		 * 
+		 * @param g		the Graphics object used for drawing
+		 */
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+		}
+		
+	} //end of DisplayPanel
+
+
+
+	/**
+	 * This inner class provides the panel on which specific dragon data is displayed
+	 */
+	private class HeaderPanel extends JPanel{
+		/**
+		 * Creates the panel.
+		 */
+		public HeaderPanel()
+		{
+			super();
+			// Necessary to set all three for fixed size.
+			this.setPreferredSize(new Dimension(1500, 150));
+			this.setMaximumSize(new Dimension(1500, 150));
+			this.setMinimumSize(new Dimension(1500, 150));
+			this.setBackground(new Color(0,255,255));
+		}
+
+		/**
+		 * Method overridden from JComponent that is responsible for
+		 * drawing components on the screen.  The supplied Graphics
+		 * object is used to draw.
+		 * 
+		 * @param g		the Graphics object used for drawing
+		 */
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+
+			// test image and text used as an example so I can remember when I come back.
+			BufferedImage image = null;
+		
+			//add placeholder dragon image
+			try {
+				image= ImageIO.read(new File("MissingNo.gif"));
+			
+			
+			}
+			catch (IOException e) {
+				System.out.println("Invalid image ABANDON SHIP\n");
+				System.out.println(e);
+			}
+		
+			g.drawImage(image, 35, 35, null);
+		
+			g.setColor(Color.black);
+			g.drawString("Test text because text", 150, 35);
+		
+		}
+		
+	} //end of DisplayPanel
+}
+
+
+// Sorry, no test Function.  Please see DAL9001.java for testing.
